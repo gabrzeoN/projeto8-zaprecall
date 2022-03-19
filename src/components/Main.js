5f62ec595711c9f42f9241ff785b6d87b8dcfa9b
Gabriel Cari <g.cari@hotmail.com>
2022-03-19 14:44:55 -0300

    Feat: Flashcard component.

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,109 +1,87 @@
+import { useState } from "react";
+
+const flashcards = [
+    {
+        name: "Pergunta 1",
+        question: "O que é JSX?",
+        answer: "Uma extensão de linguagem do JavaScript"
+    },
+    {
+        name: "Pergunta 2",
+        question: "O React é __",
+        answer: "uma biblioteca JavaScript para construção de interfaces"
+    },
+    {
+        name: "Pergunta 3",
+        question: "Componentes devem iniciar com __ ",
+        answer: "letra maiúscula"
+    },
+    {
+        name: "Pergunta 4",
+        question: "Podemos colocar __ dentro do JSX",
+        answer: "expressões"
+    },
+    {
+        name: "Pergunta 5",
+        question: "O ReactDOM nos ajuda __",
+        answer: "interagindo com a DOM para colocar componentes React na mesma"
+    },
+    {
+        name: "Pergunta 6",
+        question: "Usamos o npm para __",
+        answer: "gerenciar os pacotes necessários e suas dependências"
+    },
+    {
+        name: "Pergunta 7",
+        question: "Usamos props para __",
+        answer: "passar diferentes informações para componentes"
+    },
+    {
+        name: "Pergunta 8",
+        question: "Usamos estado (state) para __",
+        answer: "dizer para o React quais informações quando atualizadas devem renderizar a tela novamente"
+    }
+];
+
 export default function Main() {
     return (
         <main>
             <ul>
-
-                <li>
-                    <h2>Pergunta 1</h2>
-                    <ion-icon name="play-outline"></ion-icon>
-                </li>
-
-                <li>
-                    <h2>Pergunta 1</h2>
-                    <ion-icon name="play-outline"></ion-icon>
-                </li>
-
-                <li>
-                    <h2>Pergunta 1</h2>
-                    <ion-icon name="play-outline"></ion-icon>
-                </li>
-
-                <li>
-                    <h2>Pergunta 1</h2>
-                    <ion-icon name="play-outline"></ion-icon>
-                </li>
-
-                <li>
-                    <h2>Pergunta 1</h2>
-                    <ion-icon name="play-outline"></ion-icon>
-                </li>
-
-                <li>
-                    <h2>Pergunta 1</h2>
-                    <ion-icon name="play-outline"></ion-icon>
-                </li>
-
-                <li>
-                    <h2>Pergunta 1</h2>
-                    <ion-icon name="play-outline"></ion-icon>
-                </li>
-
-                <li>
-                    <h2>Pergunta 1</h2>
-                    <ion-icon name="play-outline"></ion-icon>
-                </li>
-
-                <li>
-                    <h2>Pergunta 1</h2>
-                    <ion-icon name="play-outline"></ion-icon>
-                </li>
-
-                <li>
-                    <h2>Pergunta 1</h2>
-                    <ion-icon name="play-outline"></ion-icon>
-                </li>
-
-                <li>
-                    <h2>Pergunta 1</h2>
-                    <ion-icon name="play-outline"></ion-icon>
-                </li>
-
-                <li>
-                    <h2>Pergunta 1</h2>
-                    <ion-icon name="play-outline"></ion-icon>
-                </li>
-
-                <li>
-                    <h2>Pergunta 1</h2>
-                    <ion-icon name="play-outline"></ion-icon>
-                </li>
-
-                <li>
-                    <h2>Pergunta 1</h2>
-                    <ion-icon name="play-outline"></ion-icon>
-                </li>
-
-                <li>
-                    <h2>Pergunta 1</h2>
-                    <ion-icon name="play-outline"></ion-icon>
-                </li>
-
-                <li>
-                    <h2>Pergunta 1</h2>
-                    <ion-icon name="play-outline"></ion-icon>
-                </li>
-
-                <li>
-                    <h2>Pergunta 1</h2>
-                    <ion-icon name="play-outline"></ion-icon>
-                </li>
-
-                <li>
-                    <h2>Pergunta 1</h2>
-                    <ion-icon name="play-outline"></ion-icon>
-                </li>
-
-                <li>
-                    <h2>Pergunta 1</h2>
-                    <ion-icon name="play-outline"></ion-icon>
-                </li>
-
-                <li>
-                    <h2>Pergunta 1</h2>
-                    <ion-icon name="play-outline"></ion-icon>
-                </li>
-
+                {
+                    flashcards.map(({name, question, answer}) =>
+                        <Flashcard name={name} question={question} answer={answer} />
+                    )
+                }
             </ul>
         </main>
     );
+}
+
+function Flashcard(props){
+    const {name, question, answer} = props;
+    const [flashcardStatus, setFlashCardStatus] = useState("name")
+
+    if (flashcardStatus === "name") {
+        return (
+            <li>
+                <h2>{name}</h2>
+                <ion-icon name="play-outline" onClick={() => setFlashCardStatus("question")} ></ion-icon>
+            </li>
+        );
+    } else if (flashcardStatus === "question") {
+        return (
+            <li>
+                <h2>{name}ques</h2>
+                <ion-icon name="play-outline" onClick={() => setFlashCardStatus("name")} ></ion-icon>
+            </li>
+        );
+    } else if (flashcardStatus === "answer") {
+        return (
+            <li>
+                <h2>{name}ques</h2>
+                <ion-icon name="play-outline" onClick={() => setFlashCardStatus("name")} ></ion-icon>
+            </li>
+        );
+    }
+
 }
